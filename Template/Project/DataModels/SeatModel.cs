3aceb1a423643827c7c4dc4344c5f7f1d5402e63
Arjun-adl <1098271@hr.nl>
2025-10-06 15:19:41 +0200

    kleine push

--- Template/Project/DataModels/SeatModel.cs
+++ Template/Project/DataModels/SeatModel.cs
@@ -2,7 +2,5 @@ public class SeatModel
 {
     public long SeatId { get; set; }
     public long HallId { get; set; }
-    public string? Row { get; set; }
-    public long Number { get; set; }
     public bool IsReserved { get; set; }
 }
