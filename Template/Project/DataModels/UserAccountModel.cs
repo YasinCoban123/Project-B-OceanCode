7c3739c045e9bd6e436d041a6407143714131b16
AmineArr <Aminearram2007@gmail.com>
2025-10-06 14:05:06 +0200

    Changed filename and added dateofbirth property

---
+++ Template/Project/DataModels/UserAccountModel.cs
@@ -0,0 +1,26 @@
+﻿public class UserAccountModel
+{
+
+    public Int64 Id { get; set; }
+    public string EmailAddress { get; set; }
+
+    public string Password { get; set; }
+
+    public string FullName { get; set; }
+
+    public string DateOfBirth { get; set; }
+
+    public UserAccountModel(Int64 id, string email, string password, string fullname, string dateofbirth)
+    {
+        Id = id;
+        EmailAddress = email;
+        Password = password;
+        FullName = fullname;
+        DateOfBirth = dateofbirth;
+    }
+
+
+}
+
+
+
