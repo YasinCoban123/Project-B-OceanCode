2dd2978948f21dbfbe65b1b81ef8e51c14ebe6fc
AmineArr <Aminearram2007@gmail.com>
2026-01-12 13:52:17 +0100

    added bg musics

--- Template/Project/AudioPlayer/BackgroundPlayer.cs
+++ Template/Project/AudioPlayer/BackgroundPlayer.cs
@@ -7,7 +7,21 @@ public static class BGPlayer
         @"https://drive.google.com/uc?export=download&id=1-oh7k7xUX-pWk_JKmQ5LGmRzgyKr0SHe";
 
     private static readonly string Action =
-        @"Project-B-OceanCode\Template\Project\AudioPlayer\MusicBG\racing-speed-action-music-416097.mp3";
+        @"https://drive.google.com/uc?export=download&id=1D5yzdAHv1FAfnSp_7AP_aFqTFru-Hi4j";
+
+    private static readonly string Horror =
+        @"https://drive.google.com/uc?export=download&id=12sKDfBezvHj3Mcu8XiBhalJe80oz4GfF";
+    
+    private static readonly string Documentary =
+    "https://drive.google.com/uc?export=download&id=1QB9Twje5fkyVOflkkg1vsRzN8kuTPUUI";
+
+    private static readonly string Medieval =
+    "https://drive.google.com/uc?export=download&id=1U_wKyjbw2KWjES9JbrObh9wGFFyMS53L";
+
+    private static readonly string YeSTFU =
+    "https://drive.google.com/uc?export=download&id=1SigMiGPo-LJOicfDTVLvKvJ30bzrPdmb";
+
+
 
     private static WaveOutEvent outputDevice;
     private static AudioFileReader audioFile;
@@ -26,6 +40,10 @@ public static class BGPlayer
             menu = new MenuHelper(new[] {
                 "Jazz",
                 "Action",
+                "Horror",
+                "Documentary",
+                "Medieval",
+                "YESTFU",
                 "Stop Music",
                 "Exit",
             },
@@ -44,10 +62,21 @@ public static class BGPlayer
                     break;
 
                 case 2:
+                    Play(Horror);
+                    break;
+                case 3:
+                    Play(Documentary);
+                    break;
+                case 4:
+                    Play(Medieval);
+                    break;
+                case 5:
+                    Play(YeSTFU);
+                    break;
+                case 6:
                     Stop();
                     break;
-
-                case 3:
+                case 7:
                     return; // exits program
             }
 
